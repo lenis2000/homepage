e15bf00a3e74ae4a6657053e1a1c640fb1836d00
Leonid Petrov <lenia.petrov@gmail.com>
2017-05-21 15:49:42 -0400

    update disqus

--- _includes/disqus.js
+++ _includes/disqus.js
@@ -18,5 +18,4 @@ s.setAttribute('data-timestamp', +new Date());
 </script>
 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 
-<script id="dsq-count-scr" src="//lpetrov-cc.disqus.com/count.js" async></script>
 {% endif %}
