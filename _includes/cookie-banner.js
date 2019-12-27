c318bca14066a51bb9220e81e87afcec6102c20e
Leo Petrov <lenia.petrov@gmail.com>
2019-12-27 06:58:43 -0500

    Update cookie-banner.js

--- _includes/cookie-banner.js
+++ _includes/cookie-banner.js
@@ -1,3 +1,3 @@
 <script type="text/javascript" id="cookiebanner" src="https://cdnjs.cloudflare.com/ajax/libs/cookie-banner/1.2.2/cookiebanner.min.js"
-data-message="I use cookies on this website to improve user experience."
+data-message="I am using cookies on this website"
 data-moreinfo="{{site.url}}/privacy/"></script>
