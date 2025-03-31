e75cbf7a58608d7d090cc6e7dbb9f04f9e666450
Leonid Petrov <lenia.petrov@gmail.com>
2025-03-31 06:43:30 -0400

    w

--- _simulations/domino_tilings/python_simulation.py
+++ _simulations/domino_tilings/python_simulation.py
@@ -178,138 +178,41 @@ def pretty2(m, filename):
             f.write(""+str(m[i][j])+"     ")
         f.write("\n")
 
-def compute_height_function(domino_grid):
-    size = len(domino_grid)
-
-    # Height function is defined on vertices, so it's one larger in each dimension
-    height = [[0 for _ in range(size + 1)] for _ in range(size + 1)]
-
-    # Set a reference height (e.g., top-left corner to 0)
-    height[0][0] = 0
-
-    # Compute heights systematically based on domino types
-    # First, fill in the top row and leftmost column as reference
-    for j in range(1, size + 1):
-        if j % 2 == 1:  # Odd column
-            height[0][j] = height[0][j-1]
-        else:  # Even column
-            height[0][j] = height[0][j-1]
-
-    for i in range(1, size + 1):
-        if i % 2 == 1:  # Odd row
-            height[i][0] = height[i-1][0]
-        else:  # Even row
-            height[i][0] = height[i-1][0]
-
-    # Now compute the rest of the height function
-    for i in range(1, size + 1):
-        for j in range(1, size + 1):
-            # Check the domino at (i-1, j-1) if it exists
-            if i-1 < size and j-1 < size and domino_grid[i-1][j-1] == 1:
-                # Determine domino type and apply appropriate height changes
-                if (i-1) % 2 == 0 and (j-1) % 2 == 0:  # BLUE (horizontal)
-                    if i % 2 == 1 and j % 2 == 1:  # Bottom-right corner
-                        height[i][j] = height[i-1][j-1] - 3
-                    elif i % 2 == 1 and j % 2 == 0:  # Bottom-left corner
-                        height[i][j] = height[i-1][j] - 2
-                    elif i % 2 == 0 and j % 2 == 1:  # Top-right corner
-                        height[i][j] = height[i][j-1] - 1
-                    # Top-left corner is the reference
-
-                elif (i-1) % 2 == 1 and (j-1) % 2 == 1:  # GREEN (horizontal)
-                    if i % 2 == 0 and j % 2 == 0:  # Bottom-right corner
-                        height[i][j] = height[i-1][j-1] + 3
-                    elif i % 2 == 0 and j % 2 == 1:  # Bottom-left corner
-                        height[i][j] = height[i-1][j] + 2
-                    elif i % 2 == 1 and j % 2 == 0:  # Top-right corner
-                        height[i][j] = height[i][j-1] + 1
-                    # Top-left corner is the reference
-
-                elif (i-1) % 2 == 0 and (j-1) % 2 == 1:  # RED (vertical)
-                    if i % 2 == 0 and j % 2 == 0:  # Bottom-left corner
-                        height[i][j] = height[i-1][j] + 3
-                    elif i % 2 == 0 and j % 2 == 1:  # Bottom-right corner
-                        height[i][j] = height[i][j-1] + 1
-                    elif i % 2 == 1 and j % 2 == 0:  # Top-left corner
-                        height[i][j] = height[i-1][j] + 2
-                    # Top-right corner is the reference
-
-                elif (i-1) % 2 == 1 and (j-1) % 2 == 0:  # YELLOW (vertical)
-                    if i % 2 == 1 and j % 2 == 1:  # Bottom-left corner
-                        height[i][j] = height[i-1][j] - 3
-                    elif i % 2 == 1 and j % 2 == 0:  # Bottom-right corner
-                        height[i][j] = height[i][j-1] - 1
-                    elif i % 2 == 0 and j % 2 == 1:  # Top-left corner
-                        height[i][j] = height[i-1][j] - 2
-                    # Top-right corner is the reference
-            else:
-                # No domino at this position, use the average of known neighbors
-                if height[i-1][j] is not None and height[i][j-1] is not None:
-                    height[i][j] = (height[i-1][j] + height[i][j-1]) // 2
-                elif height[i-1][j] is not None:
-                    height[i][j] = height[i-1][j]
-                elif height[i][j-1] is not None:
-                    height[i][j] = height[i][j-1]
-
-    return height
-
 def aztec_printer(x0, n):
     size = len(x0)
-
-    # Calculate height function
-    height = compute_height_function(x0)
 
     # Create Mathematica output - print to stdout
     print("Graphics[{")
 
-    # Track the number of dominoes processed
-    domino_count = 0
-    total_dominoes = sum(row.count(1) for row in x0)
-
-    # First draw all dominoes
     for i in range(size):
         for j in range(size):
             if x0[i][j] == 1:
-                domino_count += 1
-                if i % 2 == 1 and j % 2 == 1:
+                if i % 2 == 1 and j % 2 == 1:  # Green
                     color = "Green"
                     rect = f"Rectangle[{{{j - i - 2}, {size + 1 - (i + j) - 1}}}, {{{j - i - 2 + 4}, {size + 1 - (i + j) - 1 + 2}}}]"
-                elif i % 2 == 1 and j % 2 == 0:
+                elif i % 2 == 1 and j % 2 == 0:  # Blue
+                    color = "Blue"
+                    rect = f"Rectangle[{{{j - i - 1}, {size + 1 - (i + j) - 2}}}, {{{j - i - 1 + 2}, {size + 1 - (i + j) - 2 + 4}}}]"
+                elif i % 2 == 0 and j % 2 == 0:  # Red
+                    color = "Red"
+                    rect = f"Rectangle[{{{j - i - 2}, {size + 1 - (i + j) - 1}}}, {{{j - i - 2 + 4}, {size + 1 - (i + j) - 1 + 2}}}]"
+                elif i % 2 == 0 and j % 2 == 1:  # Yellow
                     color = "Yellow"
                     rect = f"Rectangle[{{{j - i - 1}, {size + 1 - (i + j) - 2}}}, {{{j - i - 1 + 2}, {size + 1 - (i + j) - 2 + 4}}}]"
-                elif i % 2 == 0 and j % 2 == 0:
-                    color = "Blue"
-                    rect = f"Rectangle[{{{j - i - 2}, {size + 1 - (i + j) - 1}}}, {{{j - i - 2 + 4}, {size + 1 - (i + j) - 1 + 2}}}]"
-                elif i % 2 == 0 and j % 2 == 1:
-                    color = "Red"
-                    rect = f"Rectangle[{{{j - i - 1}, {size + 1 - (i + j) - 2}}}, {{{j - i - 1 + 2}, {size + 1 - (i + j) - 2 + 4}}}]"
 
-                print(f"{{EdgeForm[None], {color}, {rect}}},", end="")
-
-    # Add height function values at each vertex
-    for i in range(size + 1):
-        for j in range(size + 1):
-            if height[i][j] is not None:
-                x = j - i
-                y = size + 2 - (i + j)
-
-                # Draw the height value as text at each vertex
-                vertex_drawing = f"{{Black, Text[Style[\"{height[i][j]}\", Bold, 24], {{{x}, {y}}}]}}"
-
-                # Add a point to mark the vertex
-                # point_drawing = f"{{PointSize[0.01], Point[{{{x}, {y}}}]}}"
-
-                # For all but the last vertex, add a comma
-                if i == size and j == size:
-                    print(f"{vertex_drawing}", end="")
-                else:
-                    print(f"{vertex_drawing},", end="")
+                print(f"{{EdgeForm[None], {color}, {rect}}},")
 
     # Close the Graphics list
-    print("},ImageSize->1000]")
-    print("\n")
+    print("}]")
 
-n = 4
+    # Add PlotRange and AspectRatio settings
+    min_limit = min(-size, -2)
+    max_limit = max(size, 2)
+    print(f"PlotRange -> {{{{{min_limit}, {max_limit}}}, {{{min_limit}, {max_limit + 2}}}}}")
+    print("AspectRatio -> 1")
+
+
+n = 20
 A1a = []
 for i in range(2*n):
     row = []
