131ab94a7b8e7a01ef9624edc7ab51a7c18b44f9
Leonid Petrov <lenia.petrov@gmail.com>
2025-11-27 15:06:55 -0500

    w

--- js/colorschemes.js
+++ js/colorschemes.js
@@ -9,6 +9,7 @@
  * - _simulations/lozenge_tilings/2025-06-08-q-vol-3d.md
  * - _simulations/lozenge_tilings/2025-11-26-cutout-region-glauber.md
  * - _simulations/lozenge_tilings/2025-11-26-cutout-region-q-racah.md
+ * - _simulations/lozenge_tilings/2025-11-27-lozenge-glauber.md
  * - s/domino.md
  */
 
