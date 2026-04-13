e4034578932be479dda67c28fb9243426eb566bc
Leonid Petrov <lenia.petrov@gmail.com>
2026-04-13 11:26:42 -0400

    autodeploy
    Signed-off-by: Leonid Petrov <lenia.petrov@gmail.com>

---
+++ block/sw.js
@@ -0,0 +1,42 @@
+var CACHE = 'block-v2';
+var ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(function(cache) { return cache.addAll(ASSETS); })
+      .then(function() { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    }).then(function() { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(resp) {
+        if (!resp || resp.status !== 200 || resp.type !== 'basic') return resp;
+        var copy = resp.clone();
+        caches.open(CACHE).then(function(c) { c.put(e.request, copy); });
+        return resp;
+      }).catch(function() { return cached; });
+    })
+  );
+});
